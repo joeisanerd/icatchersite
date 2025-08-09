0373cf97077ccd28f2280e395422891d4d6bc3d5
Joe Graf <joe.graf@gmail.com>
2025-08-09 07:45:57 -0700

    .

--- code/src/app/home/home.component.ts
+++ code/src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ContentCardComponent } from "../content-card/content-card.component";
-import { StoreLinkComponent } from '../store-link/store-link.component';
+import { NewHomeComponent } from "../new-home/new-home.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MatCardModule, ContentCardComponent, ContentCardComponent, StoreLinkComponent],
+  imports: [MatCardModule, ContentCardComponent, ContentCardComponent, NewHomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ code/src/app/new-home/new-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-home',
+  templateUrl: './new-home.component.html',
+  styleUrls: ['./new-home.component.scss']
+})
+export class NewHomeComponent {}
